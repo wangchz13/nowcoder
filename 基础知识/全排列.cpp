a0b49e432d949fa14ee3998895f45cc8f9d16a56
yizhixiaobai <wangchz13@users.noreply.github.com>
2016-09-03 13:40:54 +0800

    Create 全排列.cpp

---
+++ 基础知识/全排列.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+#define MAX_N 10
+int n;
+int rcd[MAX_N];
+int used[MAX_N] = {0};
+int num[MAX_N];
+void full_permutation(int l) {
+	if (l == n) {
+		for (int i = 0; i < n; i++)
+			cout << rcd[i] << " ";
+		cout << endl; return;
+	}
+	for (int i = 0; i < n; i++) {
+		if (!used[i]) {
+			used[i] = 1;
+			rcd[l] = num[i];
+			full_permutation(l + 1);
+			used[i] = 0;
+		}
+	}
+}
+int main() {
+	while (cin >> n) {
+		for (int i = 0; i < n; i++) cin >> num[i];
+		full_permutation(0);
+	}
+	return 0;
+}
